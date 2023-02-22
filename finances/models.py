fb3714ee7b9767c85a9d4f78a9329e29442e7f79
felipeholbi <4758@holbertonstudents.com>
2023-02-22 12:52:05 -0500

    models without validators

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from datetime import time
 
-# Create your models here.
+class Finanzas_Personales(models.Model):
+    ID = models.IntegerField(primary_key=True, null=False)
+    nombreCompleto = models.CharField(blank=False)
+    ingresosPersonales = models.IntegerField()
+    fechaRegistro = models.TimeField(default=time)
+    gastosMes = models.IntegerField()
+    deudasTotales = models.IntegerField()
+    pagoDeuda = models.IntegerField()
