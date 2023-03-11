7809f2950b32468f6b89f75f0524319502a00608
felipeholbi <4758@holbertonstudents.com>
2023-03-11 18:03:37 -0500

    validators, nombre y ingresos

--- finances/models.py
+++ finances/models.py
@@ -1,8 +1,14 @@
 # Modules
 from django.db import models
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from datetime import time
 
+def validador_ingresos(value):
+    maximo_permitido = 3000000
+    if value > maximo_permitido:
+        raise ValidationError(
+            message=f'el valor ingresado {value}, es mayor al permitido ')
 
 # Model
 class FinanzasPersonales(models.Model):
@@ -14,8 +20,16 @@ class FinanzasPersonales(models.Model):
     have some validators to specific only
     the values needed
     '''
-    nombreCompleto = models.CharField(blank=False, max_length=30)
-    ingresosPersonales = models.IntegerField()
+    nombreCompleto = models.CharField(
+        blank=False, 
+        max_length=30, 
+        validators=[RegexValidator(
+            regex='^[a-zA-Z ]+$', 
+            message='el formato no está permitido, solo letras')])
+    ingresosPersonales = models.IntegerField(
+        blank=False,
+        validators=[validador_ingresos],
+    )
     fechaRegistro = models.TimeField(default=time)
     gastosMes = models.IntegerField()
     deudasTotales = models.IntegerField()
